550e0d4a698952ba0ee567a4a1aa153ebcd94e38
Dave Carr <davecarr1024@gmail.com>
2023-10-19 14:30:39 -0400

    optional_result_test

--- pysh/core/parser/results/optional_result_test.py
+++ pysh/core/parser/results/optional_result_test.py
@@ -5,44 +5,96 @@ from pysh.core.parser import results
 
 class OptionalResultTest(TestCase):
     def test_no(self):
-        for result, expected in list[tuple[results.OptionalResult[int],results.NoResult[int]]]([
-(results.OptionalResult[int](), results.NoResult[int]()),
-(results.OptionalResult[int](0), results.NoResult[int]())
-        ]):
-            with self.subTest(results=result,expected=expected):
-                self.assertEqual(result.no(),expected)
+        for result, expected in list[
+            tuple[results.OptionalResult[int], results.NoResult[int]]
+        ](
+            [
+                (results.OptionalResult[int](), results.NoResult[int]()),
+                (results.OptionalResult[int](0), results.NoResult[int]()),
+            ]
+        ):
+            with self.subTest(results=result, expected=expected):
+                self.assertEqual(result.no(), expected)
 
     def test_single(self):
-        for result, expected in list[tuple[results.OptionalResult[int],Optional[results.SingleResult[int]]]]([
-(results.OptionalResult[int](), None),
-(results.OptionalResult[int](0), results.SingleResult[int](0))
-        ]):
-            with self.subTest(results=result,expected=expected):
+        for result, expected in list[
+            tuple[results.OptionalResult[int], Optional[results.SingleResult[int]]]
+        ](
+            [
+                (results.OptionalResult[int](), None),
+                (results.OptionalResult[int](0), results.SingleResult[int](0)),
+            ]
+        ):
+            with self.subTest(results=result, expected=expected):
                 if expected is None:
                     with self.assertRaises(results.Error):
                         result.single()
                 else:
-                    self.assertEqual(result.single(),expected)
+                    self.assertEqual(result.single(), expected)
 
     def test_optional(self):
-        self.assertEqual(
-            results.OptionalResult[int](0).optional(), results.OptionalResult[int](0)
-        )
+        for result, expected in list[
+            tuple[results.OptionalResult[int], Optional[results.OptionalResult[int]]]
+        ](
+            [
+                (results.OptionalResult[int](), results.OptionalResult[int]()),
+                (results.OptionalResult[int](0), results.OptionalResult[int](0)),
+            ]
+        ):
+            with self.subTest(results=result, expected=expected):
+                if expected is None:
+                    with self.assertRaises(results.Error):
+                        result.optional()
+                else:
+                    self.assertEqual(result.optional(), expected)
 
     def test_multiple(self):
-        self.assertEqual(
-            results.OptionalResult[int](0).multiple(), results.MultipleResults[int]([0])
-        )
+        for result, expected in list[
+            tuple[results.OptionalResult[int], Optional[results.MultipleResults[int]]]
+        ](
+            [
+                (results.OptionalResult[int](), results.MultipleResults[int]()),
+                (results.OptionalResult[int](0), results.MultipleResults[int]([0])),
+            ]
+        ):
+            with self.subTest(results=result, expected=expected):
+                if expected is None:
+                    with self.assertRaises(results.Error):
+                        result.multiple()
+                else:
+                    self.assertEqual(result.multiple(), expected)
 
     def test_named(self):
-        self.assertEqual(
-            results.OptionalResult[int](0).named("a"), results.NamedResults[int]({"a": 0})
-        )
+        for result, expected in list[
+            tuple[results.OptionalResult[int], Optional[results.NamedResults[int]]]
+        ](
+            [
+                (results.OptionalResult[int](), results.NamedResults[int]()),
+                (results.OptionalResult[int](0), results.NamedResults[int]({"a": 0})),
+            ]
+        ):
+            with self.subTest(results=result, expected=expected):
+                if expected is None:
+                    with self.assertRaises(results.Error):
+                        result.named("a")
+                else:
+                    self.assertEqual(result.named("a"), expected)
 
     def test_convert(self):
-        self.assertEqual(
-            results.OptionalResult[int](1).convert(
-                lambda r: results.OptionalResult[int](r.result * 2)
-            ),
-            results.OptionalResult[int](2),
-        )
+        for result, expected in list[
+            tuple[results.OptionalResult[int], results.SingleResult[int]]
+        ](
+            [
+                (results.OptionalResult[int](), results.SingleResult[int](-1)),
+                (results.OptionalResult[int](1), results.SingleResult[int](2)),
+            ]
+        ):
+            with self.subTest(result=result, expected=expected):
+                self.assertEqual(
+                    result.convert(
+                        lambda r: results.SingleResult[int](r.result * 2)
+                        if r.result is not None
+                        else results.SingleResult[int](-1)
+                    ),
+                    expected,
+                )
