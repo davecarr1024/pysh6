b6215b085586e150df432f78ff10e1808433b94d
Dave Carr <davecarr1024@gmail.com>
2023-10-31 10:56:02 -0400

    multiple and named and tests

---
+++ pysh/core/parser/rules/ands/multiple_result_and_test.py
@@ -0,0 +1,102 @@
+from unittest import TestCase
+
+from typing import Optional
+from pysh.core import errors, lexer, tokens
+
+from pysh.core.parser import results, states
+from pysh.core.parser.rules.ands import multiple_result_and
+from pysh.core.parser.rules.literals import no_result_literal, single_result_literal
+
+
+class MultipleResultAnd(TestCase):
+    def test_call(self):
+        for state, expected in list[
+            tuple[states.State[int], Optional[states.StateAndMultipleResult[int]]]
+        ](
+            [
+                (
+                    states.State(),
+                    None,
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("a", "1"),
+                            ]
+                        )
+                    ),
+                    None,
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("b", "2"),
+                            ]
+                        )
+                    ),
+                    None,
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("a", "1"),
+                                tokens.Token("b", "2"),
+                            ]
+                        )
+                    ),
+                    states.StateAndMultipleResult[int](
+                        states.State(),
+                        results.MultipleResult[int]([1, 2]),
+                    ),
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("a", "1"),
+                                tokens.Token("b", "2"),
+                                tokens.Token("c", "3"),
+                            ]
+                        )
+                    ),
+                    states.StateAndMultipleResult[int](
+                        states.State(
+                            tokens.Stream(
+                                [
+                                    tokens.Token("c", "3"),
+                                ]
+                            )
+                        ),
+                        results.MultipleResult[int]([1, 2]),
+                    ),
+                ),
+            ]
+        ):
+            with self.subTest(state=state, expected=expected):
+
+                def convert(token: tokens.Token) -> int:
+                    try:
+                        return int(token.value)
+                    except Exception as error:
+                        raise errors.Error(
+                            msg=f"failed to convert {token} to int: {error}"
+                        )
+
+                rule = multiple_result_and.MultipleResultAnd[int](
+                    [
+                        single_result_literal.SingleResultLiteral[int](
+                            lexer.Rule.load("a"),
+                            convert,
+                        ),
+                        single_result_literal.SingleResultLiteral[int](
+                            lexer.Rule.load("b"),
+                            convert,
+                        ),
+                    ]
+                )
+                if expected is None:
+                    with self.assertRaises(errors.Error):
+                        rule(state)
---
+++ pysh/core/parser/rules/ands/named_result_and_test.py
@@ -0,0 +1,114 @@
+from typing import Optional
+from unittest import TestCase
+from pysh.core import errors, lexer, tokens
+
+from pysh.core.parser import results, states
+from pysh.core.parser.rules.ands import named_result_and
+from pysh.core.parser.rules.literals import single_result_literal
+
+
+class NamedResultAndTest(TestCase):
+    def test_call(self):
+        for state, expected in list[
+            tuple[states.State[int], Optional[states.StateAndNamedResult[int]]]
+        ](
+            [
+                (
+                    states.State(),
+                    None,
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("a", "1"),
+                            ]
+                        )
+                    ),
+                    None,
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("b", "2"),
+                            ]
+                        )
+                    ),
+                    None,
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("a", "1"),
+                                tokens.Token("b", "2"),
+                            ]
+                        )
+                    ),
+                    states.StateAndNamedResult[int](
+                        states.State(),
+                        results.NamedResult[int](
+                            {
+                                "a": 1,
+                                "b": 2,
+                            }
+                        ),
+                    ),
+                ),
+                (
+                    states.State(
+                        tokens.Stream(
+                            [
+                                tokens.Token("a", "1"),
+                                tokens.Token("b", "2"),
+                                tokens.Token("c", "3"),
+                            ]
+                        )
+                    ),
+                    states.StateAndNamedResult[int](
+                        states.State(
+                            tokens.Stream(
+                                [
+                                    tokens.Token("c", "3"),
+                                ]
+                            )
+                        ),
+                        results.NamedResult[int](
+                            {
+                                "a": 1,
+                                "b": 2,
+                            }
+                        ),
+                    ),
+                ),
+            ]
+        ):
+            with self.subTest(state=state, expected=expected):
+
+                def convert(token: tokens.Token) -> int:
+                    try:
+                        return int(token.value)
+                    except Exception as error:
+                        raise errors.Error(
+                            msg=f"failed to convert {token} to int: {error}"
+                        )
+
+                rule = named_result_and.NamedResultAnd[int](
+                    [
+                        single_result_literal.SingleResultLiteral[int](
+                            lexer.Rule.load("a"),
+                            convert,
+                        ).named("a"),
+                        single_result_literal.SingleResultLiteral[int](
+                            lexer.Rule.load("b"),
+                            convert,
+                        ).named("b"),
+                    ]
+                )
+
+                if expected is None:
+                    with self.assertRaises(errors.Error):
+                        rule(state)
+                else:
+                    self.assertEqual(rule(state), expected)
